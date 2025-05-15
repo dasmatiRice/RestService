364306697b2f9d8c0dd36080bbad51e351d4761e
unknown <ansumandas@hotmail.com>
2025-05-15 12:01:14 -0400

    updated APIs to split the names and add links

--- src/main/java/com/example/payroll/Employee.java
+++ src/main/java/com/example/payroll/Employee.java
@@ -5,71 +5,86 @@ import java.util.Objects;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import lombok.Getter;
 
 @Entity
-public class Employee {
+class Employee {
 
-	private @Id @GeneratedValue Long id;
+  private @Id @GeneratedValue Long id;
+  private String firstName;
+  private String lastName;
+  private String role;
 
-	private String name;
-	private String role;
+  Employee() {}
 
-	public Employee() {
+  Employee(String firstName, String lastName, String role) {
 
-	}
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.role = role;
+  }
 
-	public Employee(String name, String role) {
+  public String getName() {
+    return this.firstName + " " + this.lastName;
+  }
 
-		this.name = name;
-		this.role = role;
+  public void setName(String name) {
+    String[] parts = name.split(" ");
+    this.firstName = parts[0];
+    this.lastName = parts[1];
+  }
 
-	}
+  public Long getId() {
+    return this.id;
+  }
 
-	public Long getId() {
-		return this.id;
-	}
+  public String getFirstName() {
+    return this.firstName;
+  }
 
-	public String getName() {
-		return this.name;
-	}
+  public String getLastName() {
+    return this.lastName;
+  }
 
-	public String getRole() {
-		return this.role;
-	}
+  public String getRole() {
+    return this.role;
+  }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+  public void setId(Long id) {
+    this.id = id;
+  }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
 
-	public void setRole(String role) {
-		this.role = role;
-	}
-	
-	
-	 @Override
-	  public boolean equals(Object o) {
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
 
-	    if (this == o)
-	      return true;
-	    if (!(o instanceof Employee))
-	      return false;
-	    Employee employee = (Employee) o;
-	    return Objects.equals(this.id, employee.id) && Objects.equals(this.name, employee.name)
-	        && Objects.equals(this.role, employee.role);
-	  }
+  public void setRole(String role) {
+    this.role = role;
+  }
 
-	  @Override
-	  public int hashCode() {
-	    return Objects.hash(this.id, this.name, this.role);
-	  }
+  @Override
+  public boolean equals(Object o) {
 
-	  @Override
-	  public String toString() {
-	    return "Employee{" + "id=" + this.id + ", name='" + this.name + '\'' + ", role='" + this.role + '\'' + '}';
-	  }
+    if (this == o)
+      return true;
+    if (!(o instanceof Employee))
+      return false;
+    Employee employee = (Employee) o;
+    return Objects.equals(this.id, employee.id) && Objects.equals(this.firstName, employee.firstName)
+        && Objects.equals(this.lastName, employee.lastName) && Objects.equals(this.role, employee.role);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(this.id, this.firstName, this.lastName, this.role);
+  }
+
+  @Override
+  public String toString() {
+    return "Employee{" + "id=" + this.id + ", firstName='" + this.firstName + '\'' + ", lastName='" + this.lastName
+        + '\'' + ", role='" + this.role + '\'' + '}';
+  }
 }
--- src/main/java/com/example/payroll/EmployeeController.java
+++ src/main/java/com/example/payroll/EmployeeController.java
@@ -1,6 +1,9 @@
 package com.example.payroll;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
@@ -13,6 +16,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.hateoas.IanaLinkRelations;
+import org.springframework.http.ResponseEntity;
 
 @RestController
 public class EmployeeController {
@@ -20,19 +25,32 @@ public class EmployeeController {
 	@Autowired
 	EmployeeRepository repository;
 	
+	@Autowired
+	EmployeeModelAssembler assembler;
+	
 	
 	 // Aggregate root
-	  // tag::get-aggregate-root[]
 	  @GetMapping("/employees")
-	  List<Employee> all() {
-	    return repository.findAll();
+	  CollectionModel<EntityModel<Employee>> all() {
+		  
+		  List<EntityModel<Employee>> employees= repository.findAll().stream()
+				  .map( assembler::toModel)
+		  .collect(Collectors.toList());
+
+		  return CollectionModel.of(employees, linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
 	  }
-	  // end::get-aggregate-root[]
+
 
 	  @PostMapping("/employees")
-	  Employee newEmployee(@RequestBody Employee newEmployee) {
-	    return repository.save(newEmployee);
+	  ResponseEntity<?> newEmployee(@RequestBody Employee newEmployee) {
+
+	    EntityModel<Employee> entityModel = assembler.toModel(repository.save(newEmployee));
+
+	    return ResponseEntity //
+	        .created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri()) //
+	        .body(entityModel);
 	  }
+	  
 	  
 	  // Single item
 	  
@@ -41,28 +59,35 @@ public class EmployeeController {
 	    
 	    Employee employee= repository.findById(id).orElseThrow(() -> new EmployeeNotFoundException(id));
 	    
-	    return EntityModel.of(employee, //
-	    	      linkTo(methodOn(EmployeeController.class).one(id)).withSelfRel(),
-	    	      linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
+	    return assembler.toModel(employee);
 	  }
 
 	  @PutMapping("/employees/{id}")
-	  Employee replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
-	    
-	    return repository.findById(id)
-	      .map(employee -> {
-	        employee.setName(newEmployee.getName());
-	        employee.setRole(newEmployee.getRole());
-	        return repository.save(employee);
-	      })
-	      .orElseGet(() -> {
-	        return repository.save(newEmployee);
-	      });
+	  ResponseEntity<?> replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
+
+	    Employee updatedEmployee = repository.findById(id) //
+	        .map(employee -> {
+	          employee.setName(newEmployee.getName());
+	          employee.setRole(newEmployee.getRole());
+	          return repository.save(employee);
+	        }) //
+	        .orElseGet(() -> {
+	          return repository.save(newEmployee);
+	        });
+
+	    EntityModel<Employee> entityModel = assembler.toModel(updatedEmployee);
+
+	    return ResponseEntity //
+	        .created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri()) //
+	        .body(entityModel);
 	  }
 
 	  @DeleteMapping("/employees/{id}")
-	  void deleteEmployee(@PathVariable Long id) {
-	    repository.deleteById(id);
+	  ResponseEntity<?> deleteEmployee(@PathVariable Long id) {
+	    
+		  repository.deleteById(id);
+		  
+		  return ResponseEntity.noContent().build();
 	  }
 
 
